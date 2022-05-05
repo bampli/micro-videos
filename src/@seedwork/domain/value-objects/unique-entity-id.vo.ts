47e40ddcf5269c7082e694177fed3acf2ab04912
Jose Motta <josemotta@bampli.com>
2022-05-05 17:31:29 -0300

    criando abstracao value object

--- src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
+++ src/@seedwork/domain/value-objects/unique-entity-id.vo.ts
@@ -1,10 +1,11 @@
 import {v4 as uuidv4, validate as uuidValidate} from "uuid";
-import InvalidUuidError from "../errors/invalid-uuid.error";
+import InvalidUuidError from "../../errors/invalid-uuid.error";
+import ValueObject from "./value-object";
 
-export default class UniqueEntityId{
+export default class UniqueEntityId extends ValueObject<string> {
 
-    constructor(public readonly id?: string) {
-        this.id = id || uuidv4();
+    constructor(readonly id?: string) {
+        super(id = id || uuidv4());
         this.validate();
     }
 
