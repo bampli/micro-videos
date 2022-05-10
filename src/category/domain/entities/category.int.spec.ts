91de6117b2695df157480072797c449d4953c679
Jose Motta <josemotta@bampli.com>
2022-05-10 18:37:50 -0300

    introducao testes integracao

---
+++ src/category/domain/entities/category.int.spec.ts
@@ -0,0 +1,25 @@
+describe('Category Integration Tests', () : void => {
+
+    it('qq coisa', () => {
+        expect(1).toBe(1);
+    })
+});
+
+// CI does 'unit tests' then 'integration tests'
+
+// Added to vscode User
+// ~/.config/Code/User/settings.json
+
+// "material-icon-theme.files.associations": {
+//     ".env.local.example": "tune",
+//     ".env.dev.example": "tune",
+//     ".env.ci.example": "tune",
+//     ".env.prod.example": "tune",
+//     ".env.production.example": "tune",
+//     ".env.testing.example": "tune",
+//     ".env.example": "tune",
+//     ".env.dusk.testing.example": "tune",
+//     "*.int-spec.ts": "test-ts",
+//     "*.ispec.ts": "test-ts",
+// },
+//"jestrunner.codeLensSelector": "**/*.{test,spec,int-spec,ispec}.{js,jsx,ts,tsx}"
