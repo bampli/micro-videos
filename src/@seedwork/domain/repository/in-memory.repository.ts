9f1550b96483f63233e79e51409234bce1b6c214
Jose Motta <josemotta@bampli.com>
2022-05-15 13:27:05 -0300

    criando abstracao InMemoryRepository 2

--- src/@seedwork/domain/repository/in-memory.repository.ts
+++ src/@seedwork/domain/repository/in-memory.repository.ts
@@ -1,4 +1,5 @@
 import Entity from "../entity/entity";
+import NotFoundError from "../errors/not-found.error";
 import UniqueEntityId from "../value-objects/unique-entity-id.vo";
 import { RepositoryInterface } from "./repository-contracts";
 
@@ -12,21 +13,31 @@ export default abstract class InMemoryRepository<E extends Entity> implements Re
 
     async findById(id: string | UniqueEntityId): Promise<E> {
         const _id = `${id}`;
-        const item = this.items.find(i => i.id === _id);
-        if(!item){
-            throw new Error('Entity not found');
-        }
-        return item;
+        return this._get(_id);
     }
     
     async findAll(): Promise<E[]> {
         return this.items;
     }
-    update(entity: E): Promise<void> {
-        throw new Error("Method not implemented.");
+    async update(entity: E): Promise<void> {
+        await this._get(entity.id);
+        const indexFound = this.items.findIndex(i => i.id === entity.id);
+        this.items[indexFound] = entity;
     }
-    delete(id: string | UniqueEntityId): Promise<void> {
-        throw new Error("Method not implemented.");
+
+    async delete(id: string | UniqueEntityId): Promise<void> {
+        const _id = `${id}`;
+        await this._get(_id);
+        const indexFound = this.items.findIndex(i => i.id === _id);
+        this.items.splice(indexFound, 1);
+    }
+
+    protected async _get(id: string): Promise<E>{
+        const item = this.items.find(i => i.id === id);
+        if(!item){
+            throw new NotFoundError(`Entity not found with ID ${id}`);
+        }
+        return item;
     }
 
 }
