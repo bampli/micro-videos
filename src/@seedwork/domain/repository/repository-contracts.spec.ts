b354d98fbbb69555b92af4a7131b874e62679255
Jose Motta <josemotta@bampli.com>
2022-05-16 18:07:36 -0300

    teste parametros busca personalizados 2

--- src/@seedwork/domain/repository/repository-contracts.spec.ts
+++ src/@seedwork/domain/repository/repository-contracts.spec.ts
@@ -51,4 +51,76 @@ describe('SearchParams Unit Tests', () => {
             expect(new SearchParams({ per_page: i.per_page as any }).per_page).toBe(i.expected);
         })
     });
+
+    test('sort prop', () => {
+        const params = new SearchParams();
+        expect(params.sort).toBeNull();
+
+        const arrange = [
+            { sort: null, expected: null },
+            { sort: undefined, expected: null },
+            { sort: "", expected: null },
+            { sort: 0, expected: "0" },
+            { sort: -1, expected: "-1" },
+            { sort: 5.5, expected: "5.5" },
+            { sort: true, expected: "true" },
+            { sort: false, expected: "false" },
+            { sort: {}, expected: "[object Object]" },
+            { sort: "field", expected: "field" },
+        ];
+
+        arrange.forEach((i) => {
+            expect(new SearchParams({ sort: i.sort as any }).sort).toBe(i.expected);
+        })
+    });
+
+    test('sort_dir prop', () => {
+        let params = new SearchParams();
+        expect(params.sort_dir).toBeNull();
+
+        params = new SearchParams({sort: undefined});
+        expect(params.sort_dir).toBeNull();
+
+        params = new SearchParams({sort: ""});
+        expect(params.sort_dir).toBeNull();
+        
+        const arrange = [
+            { sort_dir: null, expected: "asc" },
+            { sort_dir: undefined, expected: "asc" },
+            { sort_dir: "", expected: "asc" },
+            { sort_dir: 0, expected: "asc" },
+            { sort_dir: "fake", expected: "asc" },
+
+            { sort_dir: "asc", expected: "asc" },
+            { sort_dir: "ASC", expected: "asc" },
+            { sort_dir: "desc", expected: "desc" },
+            { sort_dir: "DESC", expected: "desc" },
+        ];
+
+        arrange.forEach((i) => {
+            expect(new SearchParams({ sort: "field", sort_dir: i.sort_dir as any }).sort_dir).toBe(i.expected);
+        })
+    });
+
+    test('filter prop', () => {
+        const params = new SearchParams();
+        expect(params.filter).toBeNull();
+
+        const arrange = [
+            { filter: null, expected: null },
+            { filter: undefined, expected: null },
+            { filter: "", expected: null },
+            { filter: 0, expected: "0" },
+            { filter: -1, expected: "-1" },
+            { filter: 5.5, expected: "5.5" },
+            { filter: true, expected: "true" },
+            { filter: false, expected: "false" },
+            { filter: {}, expected: "[object Object]" },
+            { filter: "field", expected: "field" },
+        ];
+
+        arrange.forEach((i) => {
+            expect(new SearchParams({ filter: i.filter as any }).filter).toBe(i.expected);
+        })
+    });
 });
