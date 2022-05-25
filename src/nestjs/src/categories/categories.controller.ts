820ccf1c56fe9d44378eed0f233b07ae01aaf833
Jose Motta <josemotta@bampli.com>
2022-05-25 17:15:23 -0300

    o necessario num modulo nest.js para DDD

--- src/nestjs/src/categories/categories.controller.ts
+++ src/nestjs/src/categories/categories.controller.ts
@@ -1,11 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(private readonly categoriesService: CategoriesService) { }
 
   @Post()
   create(@Body() createCategoryDto: CreateCategoryDto) {
