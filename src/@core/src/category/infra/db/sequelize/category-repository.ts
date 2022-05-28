3cf1570c9919c7ec9132f8ef92215b1ea6b8137b
Jose Motta <josemotta@bampli.com>
2022-05-28 14:01:41 -0300

    implementando insert no repositorio

--- src/@core/src/category/infra/db/sequelize/category-repository.ts
+++ src/@core/src/category/infra/db/sequelize/category-repository.ts
@@ -1,15 +1,26 @@
 import { UniqueEntityId } from "#seedwork/domain";
 import { CategoryRepository, Category } from "#category/domain";
+import { CategoryModel } from "./category-model";
 
 
-export class CategorySequelizeRepository implements CategoryRepository.Repository {
-    sortableFields: string[];
-    async insert(entity: Category): Promise<void> { }
+export class CategorySequelizeRepository
+    implements CategoryRepository.Repository {
+
+    sortableFields: string[] = ['name', 'created_at'];
+
+    constructor(private categoryModel: typeof CategoryModel) { }
+
+    async insert(entity: Category): Promise<void> {
+        await this.categoryModel.create(entity.toJSON());
+    }
+    //@ts-expect-error
     async findById(id: string | UniqueEntityId): Promise<Category> { }
+    //@ts-expect-error
     async findAll(): Promise<Category[]> { }
     async update(entity: Category): Promise<void> { }
     async delete(id: string | UniqueEntityId): Promise<void> { }
     async search(
         props: CategoryRepository.SearchParams
+        //@ts-expect-error
     ): Promise<CategoryRepository.SearchResult> { }
 }
