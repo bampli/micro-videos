deb9ca2c834edd144df4ee693b1421374da99b1b
Jose Motta <josemotta@bampli.com>
2022-09-30 12:05:06 -0300

    criando presenter para serializar busca categorias - 1

--- src/nestjs/src/categories/presenter/category.presenter.ts
+++ src/nestjs/src/categories/presenter/category.presenter.ts
@@ -1,4 +1,7 @@
-import { CategoryOutput } from '@fc/micro-videos/category/application';
+import {
+  CategoryOutput,
+  ListCategoriesUseCase,
+} from '@fc/micro-videos/category/application';
 import { Transform } from 'class-transformer';
 
 export class CategoryPresenter {
@@ -17,3 +20,47 @@ export class CategoryPresenter {
     this.created_at = output.created_at;
   }
 }
+
+// it will be moved to @fc/micro-videos later
+export type PaginationPresenterProps = {
+  current_page: number;
+  per_page: number;
+  last_page: number;
+  total: number;
+};
+
+export class PaginationPresenter {
+  @Transform(({ value }) => parseInt(value))
+  current_page: number;
+  @Transform(({ value }) => parseInt(value))
+  per_page: number;
+  @Transform(({ value }) => parseInt(value))
+  last_page: number;
+  @Transform(({ value }) => parseInt(value))
+  total: number;
+
+  constructor(props: PaginationPresenterProps) {
+    this.current_page = props.current_page;
+    this.per_page = props.per_page;
+    this.last_page = props.current_page;
+    this.total = props.total;
+  }
+}
+
+export class CategoryCollectionPresenter {
+  // sugestão reuso
+  // constructor(output: CategoryOutput[], paginationProps){}
+
+  constructor(output: ListCategoriesUseCase.Output) {}
+}
+
+// API spec:
+// {
+//   data: [{}, {}, {}],
+//   meta: {
+//     total,
+//     current_page,
+//     per_page,
+//     last_page
+//   }
+// }
