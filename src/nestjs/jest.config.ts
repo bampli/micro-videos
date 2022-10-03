299b374d659c36cfe5c86a9d481d1ac792167091
Jose Motta <josemotta@bampli.com>
2022-10-03 17:23:28 -0300

    acertando configuracoes extensao Orta Jest

--- src/nestjs/jest.config.ts
+++ src/nestjs/jest.config.ts
@@ -4,7 +4,6 @@ export default {
     color: 'magentaBright',
   },
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'src',
   testRegex: '.*\\..*spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': '@swc/jest',
@@ -16,18 +15,16 @@ export default {
   testEnvironment: 'node',
   moduleNameMapper: {
     '@fc/micro\\-videos/(.*)$':
-      '<rootDir>/../../../node_modules/@fc/micro-videos/dist/$1',
-    //'#seedwork/domain': '<rootDir>/../../../node_modules/@fc/micro-videos/dist/@seedwork/domain/index.js',
+      '<rootDir>/../../node_modules/@fc/micro-videos/dist/$1',
+    //'#seedwork/domain': '<rootDir>/../../node_modules/@fc/micro-videos/dist/@seedwork/domain/index.js',
     '#seedwork/(.*)$':
-      '<rootDir>/../../../node_modules/@fc/micro-videos/dist/@seedwork/$1',
-    //'#category/domain': '<rootDir>/../../../node_modules/@fc/micro-videos/dist/category/domain/index.js',
+      '<rootDir>/../../node_modules/@fc/micro-videos/dist/@seedwork/$1',
+    //'#category/domain': '<rootDir>/../../node_modules/@fc/micro-videos/dist/category/domain/index.js',
     '#category/(.*)$':
-      '<rootDir>/../../../node_modules/@fc/micro-videos/dist/category/$1',
+      '<rootDir>/../../node_modules/@fc/micro-videos/dist/category/$1',
   },
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  setupFilesAfterEnv: [
-    '../../@core/src/@seedwork/domain/tests/jest.ts', // due to supposed issue with 'fail' at this jest version
-  ],
+  setupFilesAfterEnv: ['../@core/src/@seedwork/domain/tests/jest.ts'],
   // An object that configures minimum threshold enforcement for coverage results
   coverageThreshold: {
     global: {
