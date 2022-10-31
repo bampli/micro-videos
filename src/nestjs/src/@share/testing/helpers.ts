681150617970444408f26889bfcad0dd21d50b95
Jose Motta <josemotta@bampli.com>
2022-10-31 12:02:27 -0300

    teste -- busca caegoria e2e - 1

--- src/nestjs/src/@share/testing/helpers.ts
+++ src/nestjs/src/@share/testing/helpers.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../../app.module';
 import { applyGlobalConfig } from '../../global-config';
+import { getConnectionToken } from '@nestjs/sequelize';
 
 export function startApp({
   beforeInit,
@@ -9,10 +10,12 @@ export function startApp({
   let _app: INestApplication;
 
   beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+    const moduleBuilder: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
-    _app = moduleFixture.createNestApplication();
+    const sequelize = moduleBuilder.get(getConnectionToken());
+    await sequelize.sync({ force: true }); // clear db
+    _app = moduleBuilder.createNestApplication();
     applyGlobalConfig(_app);
     beforeInit && beforeInit(_app);
     await _app.init();
